34f1b2dd8bdb45f842786298382cd4ec730bd1c7
vjp-walterML <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2023-10-05 19:34:46 +0200

    clase

--- EjerciciosHilos5678/src/ejercicio8/Main.java
+++ EjerciciosHilos5678/src/ejercicio8/Main.java
@@ -14,7 +14,21 @@ public class Main {
     private static int[][] matriz2 = {{4, 2, 1}, {9, 8, 1}, {1, 1, 0}};
     private static int[][] matrizResultado = new int[3][3];
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Hilo hilo = new Hilo(matriz1[i][j], matriz2[i][j]);
+                hilo.start();
+                hilo.join();
+                matrizResultado[i][j] = hilo.getSumaTotal();
+            }
+        }
 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrizResultado[i][j] + "\t");
+            }
+            System.out.println("");
+        }
     }
 }
