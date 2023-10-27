01e709c5e219e298ef44e12df52276dc2faeaf4e
vjp-walterML <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2023-10-27 18:06:30 +0200

    casa

--- UT1.Hilos y sincronizaciones en Java/UT01_Ejercicios_16_17_18/src/ejercicio16/Persona.java
+++ UT1.Hilos y sincronizaciones en Java/UT01_Ejercicios_16_17_18/src/ejercicio16/Persona.java
@@ -1,17 +1,42 @@
 package ejercicio16;
 
+import java.util.Random;
+
 public class Persona extends Thread {
     private Wc wc;
     private String nombre;
 
-//    Constructor
+    //    Constructor
     public Persona(Wc wc, String nombre) {
         this.wc = wc;
         this.nombre = nombre;
     }
 
+    //    Métodos propios
+    public int tiempoAleatorio() {
+//        Este método retorna una cantidad aleatoria de ms entre 5 y 20ms
+        return new Random().nextInt((20 + 1) - 5) + 5;
+    }
+
     @Override
     public void run() {
-        //ACCIONES
+        for (int i = 0; i < 3; i++) {
+//        Entrar
+            wc.entrar(this);
+//        Tiempo aleatorio dentro
+            try {
+                sleep(tiempoAleatorio());
+            } catch (InterruptedException e) {
+                //Nada
+            }
+//        Salir
+            wc.salir(this);
+//        Espera de 50ms después de ir al baño
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                //Nada
+            }
+        }
     }
 }
